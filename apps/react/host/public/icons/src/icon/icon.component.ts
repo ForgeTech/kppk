d2d9feb21d6ba257c384f6663b8fe8491f51bb32
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-08 13:42:13 +0100

    Added host assets

---
+++ apps/react/host/public/icons/src/icon/icon.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fg-icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.scss'],
+})
+export class IconComponent {}
---
+++ apps/react/host/public/icons/src/icon/icon.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IconComponent } from './icon.component';
+
+@NgModule({
+  declarations: [IconComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [IconComponent],
+})
+export class IconModule {}
