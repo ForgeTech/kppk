8536e30193de3ab80ce3d2f191325a9a662ad001
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-09 07:27:16 +0100

    Added service to fg-lib

---
+++ libs/fg-lib/src/lib/base/fg-base.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, OnDestroy, OnInit, inject } from '@angular/core';
+import { NGXLogger } from 'ngx-logger';
+/**
+ * FgBaseLogService -
+ * Meant to be extenden by FgServices to provide common service
+ * functionality
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class FgBaseService implements OnDestroy {
+  /** Holds the classes name taken from the constructor
+   * CAUTION: This can vary between envirments depending on possible
+   * javascript uglification etc. So don't use if for example
+   * this.classname === 'className' comparissions
+   */
+  public readonly className: string = this.constructor.name;
+
+  /** (Optional) Provide logger service */
+  protected $log = inject(NGXLogger);
+  
+  constructor() {
+    this?.$log.info('SERVICE: ', this.className, 'created!');  
+  }
+  
+  public ngOnDestroy(): void {
+    this?.$log.info('SERVICE: ', this.className, 'destoryed!');  
+  }
+}
