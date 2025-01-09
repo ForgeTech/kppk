957732d1fb9240360ec43dca510180eb871bb030
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-09 12:58:53 +0100

    Refector

--- apps/react/host/src/app/app.component.spec.ts
+++ apps/react/host/src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { ReactLibComponent } from '@kppk/react-lib'
+import { PlaceholderComponent  } from '@forgetech/fg-lib'
 import { Router, RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
@@ -10,11 +10,11 @@ describe('AppComponent', () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterModule.forRoot([
-          { path: '', component: ReactLibComponent },
+          { path: '', component: PlaceholderComponent },
           {
             outlet: 'print-outlet',
             path: 'print',
-            component: ReactLibComponent
+            component: PlaceholderComponent
           },
         ]),
         AppComponent,
@@ -30,7 +30,7 @@ describe('AppComponent', () => {
     });
   
     it(`should have as title 'react-host'`, () => {
-      expect(app.title).toEqual('react-host');
+      expect(app.title).toEqual('kppk-react');
     });
     
   });
@@ -47,95 +47,9 @@ describe('AppComponent', () => {
       template = fixture.nativeElement as HTMLElement;
     }));
 
-    describe('>> section ".page-content"', () => {
-      let pageContentSections: HTMLCollectionOf<Element>
-      beforeEach( () => {
-        pageContentSections = template.getElementsByClassName('page-content')
-      })
-      it('single instance exists', () => {
-        expect(pageContentSections.length).toBe(1);
-        expect(pageContentSections[0].tagName.toLowerCase()).toBe('section');
-      });
-      it('contains single instance of router-outlet ".content-outlet"', () => {
-        const routerOutlets = pageContentSections[0].getElementsByTagName('router-outlet');
-        expect(routerOutlets.length).toBe(1);
-        expect(routerOutlets[0].classList.contains('content-outlet')).toBeTruthy();
-      });
-    });
-
-    describe('>> section ".print-content"', () => {
-      let printContentSections: HTMLCollectionOf<Element>
-      beforeEach( () => {
-        printContentSections = template.getElementsByClassName('print-content')
-      })
-      it('single instance exists', () => {
-        expect(printContentSections.length).toBe(1);
-        expect(printContentSections[0].tagName.toLowerCase()).toBe('section');
-      });
-      it('contains single instance of router-outlet ".print-outlet"', () => {
-        const routerOutlets = printContentSections[0].getElementsByTagName('router-outlet');
-        expect(routerOutlets.length).toBe(1);
-        expect(routerOutlets[0].classList.contains('print-outlet')).toBeTruthy();
-      });
-      it('router-outlet ".print-outlet" has name of "print-outlet"', () => {
-        const routerOutlets = printContentSections[0].getElementsByTagName('router-outlet');
-        expect(routerOutlets[0].hasAttribute('name')).toBeTruthy();
-        expect(routerOutlets[0].getAttribute('name')).toBe('print-outlet');
-      });
-    });
-
-  });
-
-  describe( '>> behaviour', () => {
-
-    describe('>> when only default router-outlet is active', () => {
-      let template: HTMLElement;
-      beforeEach(fakeAsync(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const router = TestBed.inject(Router);
-        fixture.ngZone?.run(() => router.navigate(['']));
-        tick();
-        fixture.detectChanges();
-        template = fixture.nativeElement as HTMLElement;
-      }));
-      it('section ".page-content" is visible', () => {
-        const pageContentSections = template.getElementsByClassName('page-content');
-        expect(pageContentSections.length).toBe(1);
-        expect(pageContentSections[0].classList.contains('invisible')).toBeFalsy();
-      })
-      it('section ".print-content" is invisible', () => {
-        const printContentSections = template.getElementsByClassName('print-content');
-        expect(printContentSections.length).toBe(1);
-        expect(printContentSections[0].classList.contains('invisible')).toBeTruthy();
-      })
-    });
- 
-    describe('>> when default and ".print-outlet" router-outlets are active', () => {
-      let template: HTMLElement;
-      beforeEach(fakeAsync(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const router = TestBed.inject(Router);
-        fixture.ngZone?.run(() => router.navigate(['', {
-          outlets: {
-            'print-outlet': ['print']
-          }
-        }]));
-        tick();
-        fixture.detectChanges();
-        template = fixture.nativeElement as HTMLElement;
-      }));
-      it('section ".page-content" is invisible', () => {
-        const pageContentSections = template.getElementsByClassName('page-content');
-        expect(pageContentSections.length).toBe(1);
-        expect(pageContentSections[0].classList.contains('invisible')).toBeTruthy();
-      })
-      it('section ".print-content" is visible', () => {
-        const printContentSections = template.getElementsByClassName('print-content');
-        expect(printContentSections.length).toBe(1);
-        expect(printContentSections[0].classList.contains('invisible')).toBeFalsy();
-      })
-    });
-
+    it('contains single instance of router-outlet', () => {
+      const outlets = template.getElementsByTagName('router-outlet');
+      expect(outlets.length).toBe(1);
+    })
   })
-
 });
--- apps/react/host/src/app/app.component.ts
+++ apps/react/host/src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { NgClass } from '@angular/common';
 import {  Component, signal, ViewEncapsulation } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PlaceholderComponent } from '@forgetech/fg-lib';
 
 @Component({
-  imports: [RouterModule, NgClass],
+  imports: [RouterModule, PlaceholderComponent],
   selector: 'kppk-react-root',
   template: `
     <ul class="remote-menu">
@@ -12,31 +12,11 @@ import { RouterModule } from '@angular/router';
       <li><a routerLink="react_view_home">ReactViewHome</a></li>
       <li><a routerLink="react_view_calc">ReactViewCalc</a></li>
     </ul>
-    <section 
-      class="page-content"
-      [ngClass]="{
-        'invisible': printOutletActivated_s() === true
-      }"
-    >
-      <router-outlet class="content-outlet"/>
-    </section>
-    <section class="print-content"
-      [ngClass]="{
-        'invisible': printOutletActivated_s() === false
-      }"
-    >
-      <router-outlet 
-        class="print-outlet" 
-        name="print-outlet"
-        (activate)="printOutletActivated_s.set(true)"
-        (deactivate)="printOutletActivated_s.set(false)"
-      />
-    </section>
+    <router-outlet/>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  public title = 'react-host';
-  protected printOutletActivated_s = signal(false);
+  public title = 'kppk-react';
 }
