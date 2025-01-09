6c4a69c233b65213816f37c32dcb240851db3de5
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-09 17:56:51 +0100

    Delete outdated formly types

--- libs/fg-lib/src/lib/token/fg-environment.token.ts
+++
@@ -1,7 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { FgEnvironmentConfigInterface } from '../interface/fg-environment.config.interface';
-
-/**
- * Injection-Token used used to provide environment variables outside app.module
- */
-export const FG_ENVIRONMENT = new InjectionToken<FgEnvironmentConfigInterface<any>>('FgEnvironment');
