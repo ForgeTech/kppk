d74d0b11fc933d8b64ec14c98fb56aa0c1e4205d
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-10 10:04:22 +0100

    Refector

---
+++ apps/react/host/src/interface/app.config.interface.ts
@@ -0,0 +1,8 @@
+import { FgAppConfigInterface } from '@kppk/fg-lib';
+
+import { AppModelInterface } from './app.model.interface';
+/**
+ * AppConfigInterface -
+ * Interface describes a common application data configuration
+ */
+export interface AppConfigInterface extends FgAppConfigInterface<any, any, AppModelInterface> {}
