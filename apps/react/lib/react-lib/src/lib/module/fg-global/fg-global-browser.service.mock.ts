cc9d2a20f831425b51d31844421297e0dd6c325a
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-12 12:15:44 +0100

    Removed fg-global module

--- apps/react/lib/react-lib/src/lib/module/fg-global/fg-global-browser.service.mock.ts
+++
@@ -1,5 +0,0 @@
-import { createSpyFromClass } from 'jest-auto-spies';
-import { FgBrowserGlobalService } from './fg-global-browser.service';
-
-/** FgGlobalBrowserServiceMock - Turn FgBrowserGlobalService into a mock version by wrapping it into jasmine-spies */
-export const FgGlobalBrowserServiceMock = createSpyFromClass(FgBrowserGlobalService);
--- apps/react/lib/react-lib/src/lib/module/fg-global/fg-global.service.ts
+++
@@ -1,7 +0,0 @@
-/** FgGlobalService -
- * Abstact Service-class to extend by environment implementation
- */
-export abstract class FgGlobalService {
-  abstract isBrowser: boolean;
-  abstract nativeGlobal<T>(): T;
-}
