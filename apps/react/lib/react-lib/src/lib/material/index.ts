d08d7feec15923ef255633c2b7fb679b5c32ea15
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-17 10:52:25 +0100

    Removed material mock component folder

--- apps/react/lib/react-lib/src/lib/material/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './address-form/address-form.component';
-export * from './dashboard/dashboard.component';
-export * from './navigation/navigation.component';
-export * from './table/table.component';
-export * from './tree/tree.component';
