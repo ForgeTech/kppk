0d2e49ab302738c93ca8b47da98ad4223b2b3fe3
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-17 16:03:15 +0100

    Moved/Removed assets

--- apps/react/host/public/assets/icons/src/main-icon.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { IconModule } from './icon/icon.module';
-platformBrowserDynamic()
-  .bootstrapModule(IconModule)
-  .catch(err => console.error(err));
