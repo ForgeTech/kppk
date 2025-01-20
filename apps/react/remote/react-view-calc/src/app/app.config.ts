045ca3f1de688d1ab8353f9415242c9870f01108
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-20 12:33:12 +0100

    Updated remote app configs

--- apps/react/remote/react-view-calc/src/app/app.config.ts
+++ apps/react/remote/react-view-calc/src/app/app.config.ts
@@ -1,15 +1,42 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTransloco } from '@jsverse/transloco';
+import { TranslocoHttpLoader } from '@kppk/react-lib';
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+import { FG_ENVIRONMENT, FgEnvironmentService } from '@kppk/fg-lib-new';
+import { environment } from './../environments/environment';
+import { CookieModule } from 'ngx-cookie';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
     provideExperimentalZonelessChangeDetection(),
     provideRouter(appRoutes),
+    provideHttpClient(),
+    provideAnimations(),
+    provideTransloco({
+      config: {
+        availableLangs: environment.i18n.availableLangs,
+        defaultLang: environment.i18n.defaultLang,
+        reRenderOnLangChange: environment.i18n.reRenderOnLangChange,
+        prodMode: environment.i18n.prodMode,
+      },
+      loader: TranslocoHttpLoader,
+    }),
+    importProvidersFrom(LoggerModule.forRoot({
+      // serverLoggingUrl: '/api/logs',
+      level: NgxLoggerLevel.DEBUG,
+      serverLogLevel: NgxLoggerLevel.ERROR
+    })),
+    FgEnvironmentService,
+    { provide: FG_ENVIRONMENT, useValue: environment },
+    importProvidersFrom(CookieModule.withOptions({})),
   ],
 };
--- apps/react/remote/react-view-login/src/app/app.config.ts
+++ apps/react/remote/react-view-login/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom, isDevMode, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import {
@@ -10,7 +10,7 @@ import { provideTransloco } from '@jsverse/transloco';
 import { TranslocoHttpLoader } from '@kppk/react-lib';
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 import { FG_ENVIRONMENT, FgEnvironmentService } from '@kppk/fg-lib-new';
-import { environment } from './../../../../host/src/environments/environment';
+import { environment } from './../environments/environment';
 import { CookieModule } from 'ngx-cookie';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
