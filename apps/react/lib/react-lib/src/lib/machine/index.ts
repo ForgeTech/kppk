06aff06ac41578010a6eee2d442f2ac050d3b5e7
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-22 12:16:54 +0100

    Added react-view-home export

--- apps/react/lib/react-lib/src/lib/machine/index.ts
+++ apps/react/lib/react-lib/src/lib/machine/index.ts
@@ -2,4 +2,5 @@ export * from './fg-auth-local';
 export * from './fg-spinner';
 export * from './react-init';
 export * from './react-admin-toolbar';
+export * from './react-view-home';
 export * from './machine.utils';
