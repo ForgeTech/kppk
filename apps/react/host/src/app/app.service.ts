472a3a13bfc8308e3d8fa72124892b6da3d1babf
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-22 07:00:58 +0100

    Added breakpoint service

--- apps/react/host/src/app/app.service.ts
+++ apps/react/host/src/app/app.service.ts
@@ -1,9 +1,10 @@
 import { ApplicationRef, inject, Injectable, signal } from '@angular/core';
-import { FgBaseService } from '@kppk/fg-lib-new';
+import { FgBaseService, FgBreakpoint } from '@kppk/fg-lib-new';
 import { EventFgSpinnerHideParser,
   EventFgSpinnerShowParser,
   FgAuthLocalMachineActorService,
-  FgSpinnerMachineActorService, 
+  FgSpinnerMachineActorService,
+  ReactInitMachineActorService, 
 } from '@kppk/react-lib';
 
 /**
@@ -18,12 +19,13 @@ export class AppService extends FgBaseService {
   protected $app_ref = inject(ApplicationRef);
   protected $actor_spinner = inject(FgSpinnerMachineActorService);
   protected $actor_auth = inject(FgAuthLocalMachineActorService);
-
+  protected $breakpoint = inject(FgBreakpoint);
 
   public readonly app_readyS = signal(false);
   // CONSTRUCTOR
   constructor() { 
     super();
+
     const event_force_spinner_show = EventFgSpinnerShowParser.parse({ payload: { force: true }})
     this.$actor_spinner.start();
     this.$actor_spinner.send(event_force_spinner_show);
@@ -34,5 +36,15 @@ export class AppService extends FgBaseService {
       const event_spinner_hide = EventFgSpinnerHideParser.parse({})
       this.$actor_spinner.send(event_spinner_hide);
     });
+
+    const custom_breakpoints = {
+      'xs': 'screen and (min-width: 320px)',
+      'sm': 'screen and (min-width: 640px)',
+      'md': 'screen and (min-width: 768px)',
+      'lg': 'screen and (min-width: 1024px)',
+      'xl': 'screen and (min-width: 1280px)',
+      '2xl': 'screen and (min-width: 1536px)'
+    }
+    this.$breakpoint.register(custom_breakpoints);
   }
 }
