6178f9dd29bbae5493e933532f6607ba4bf7a547
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-22 07:12:02 +0100

    Added and migrating admin toolbar WIP

--- apps/react/lib/react-lib/src/lib/component/kppk-admin-toolbar/kppk-admin-toolbar.component.ts
+++ apps/react/lib/react-lib/src/lib/component/kppk-admin-toolbar/kppk-admin-toolbar.component.ts
@@ -1,12 +1,14 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation, computed, effect, inject, isDevMode, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation, computed, effect, inject, input, signal, untracked } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { REACT_VIEW_CALCULATION_CONTEXT } from '../../types/kppk-react-calculation.types';
 import { CommonModule } from '@angular/common';
-import { FgAuthLocalMachineActorService, ReactInitMachineActorService } from '../../machine';
+import { ReactAdminToolbarMachineActorService, ReactInitMachineActorService } from '../../machine';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FgTranslate } from '@kppk/fg-lib-new';
+import { FgXstateService } from '../../service';
 
 /**
  * KppkAdminToolbarComponent -
@@ -29,67 +31,79 @@ import { FgAuthLocalMachineActorService, ReactInitMachineActorService } from '..
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class KppkAdminToolbarComponent  {
-  protected $auth = inject(FgAuthLocalMachineActorService);
-  protected $init = inject(ReactInitMachineActorService);
+  protected $actor_admin_toolbar = inject(ReactAdminToolbarMachineActorService);
+  protected $actor_init = inject(ReactInitMachineActorService);
+  protected $translate = inject(FgTranslate);
+  protected $xstate = inject(FgXstateService);
 
-  public show_admin_toolbarS = computed( () => {
-    return isDevMode() || this.$auth.stateS()?.matches({'STATE': 'AUTHORIZED'}) ? true : false;;
+  public showS = input<boolean>(false, {alias: 'show'});
+
+  protected disabled_test_calculationS = computed( () => {
+    let disabled = true;
+    const status = this.$actor_init.stateS()?.status;
+    if( status ) {
+      disabled = status === 'done';
+    } 
+    return disabled;
   });
+
+  protected translationS = toSignal(this.$translate.get_translations$({
+    "alt_xstate": "admin",
+    "development_tools": "admin",
+    "test_calculation": "admin",
+    "tooltip_test_calculation": "admin",
+    "tooltip_xstate": "admin",
+  }), { initialValue: undefined });
+
   protected xstate_is_onS = computed( () => {
-    // return this.state_react_running_admin_toolbar_s()?.matches({'X_STATE': 'ON'}) ? true : false;
+    return this.$actor_admin_toolbar.stateS()?.matches({'X_STATE': 'ON'}) ? true : false;
   });
   protected test_calculation_is_onS = computed( () => {
-    // return this.state_react_running_admin_toolbar_s()?.matches({'TEST_CALCULATION': 'ON'}) ? true : false;
+    return this.$actor_admin_toolbar.stateS()?.matches({'TEST_CALCULATION': 'ON'}) ? true : false;
   });
   protected inspector_activesS = signal(false);
 
   constructor(){
-    if( this.$init.is_runningS() === false ) {
-      this.$init.start();
+    this.$actor_admin_toolbar.start()
+    if( this.$actor_init.is_runningS() === false ) {
+      this.$actor_init.start();
     }
-    // effect( () => {
-    //   const show_admin_toolbar_s = this.show_admin_toolbar_s();
-    //   console.log('>>>>>>>>>>>>>>SHOW_ADMIN_TOOLBAR>>>>>>>>');
-    //   console.log(show_admin_toolbar_s);
-    // });
     effect( () => {
-      const init = this.$init.stateS();
+      const init = this.$actor_admin_toolbar.stateS();
       if( init ) {
-        console.log('>>>>>>>>>>>>>>SHOW_INIT_STATE>>>>>>>>');
+        console.log('>>>>>>>>>>>>>>ADMIN_TOOLBAR>>>>>>>>');
         console.log(init.context);
       }
     });
-    // effect( () => {
-    //   // console.log('>>>>>>>>>>>>>>XSTATE>>>>>>>>>>>');
-    //   // console.log( this.xstate_is_on_s() );
-    //   if( this.xstate_is_on_s() ) {
-    //     this.$xstate.start();
-    //   } else {
-    //     this.$xstate.stop();
-    //   }
-    // })
-    // effect( () => {
-    //     // console.log('>>>>>>>>>>>>>>TEST_CALCILATION>>>>>>>>>>>');
-    //     // console.log( this.test_calculation_is_on_s() );
-    //     const test_calculation_is_on = this.test_calculation_is_on_s();
-    //     untracked(() => {
-    //       const payload = this.state_react_running_s()?.context.init_output?.debug_calculation_v1;
-    //       const calculation = this.state_react_running_s()?.context.calculation;
-    //       if( test_calculation_is_on === true && calculation === undefined && payload  ) {
-    //         const event: { type: "react.running.event.calculation.start",  payload: REACT_VIEW_CALCULATION_CONTEXT } = { type: 'react.running.event.calculation.start', payload }
-    //         this.actor_react_running_s()?.send(event);
-    //       } else if ( test_calculation_is_on === false) {
-    //         this.actor_react_running_s()?.send({ type: 'react.running.event.calculation.cancel'});
-    //       }
-    //     });
+    effect( () => {
+      if( this.xstate_is_onS() ) {
+        this.$xstate.start();
+      } else {
+        this.$xstate.stop();
+      }
+    })
+    effect( () => {
+        // console.log('>>>>>>>>>>>>>>TEST_CALCILATION>>>>>>>>>>>');
+        // console.log( this.test_calculation_is_on_s() );
+        const test_calculation_is_on = this.test_calculation_is_onS();
+        untracked(() => {
+          // const payload = this.state_react_running_s()?.context.init_output?.debug_calculation_v1;
+          // const calculation = this.state_react_running_s()?.context.calculation;
+          // if( test_calculation_is_on === true && calculation === undefined && payload  ) {
+          //   const event: { type: "react.running.event.calculation.start",  payload: REACT_VIEW_CALCULATION_CONTEXT } = { type: 'react.running.event.calculation.start', payload }
+          //   this.actor_react_running_s()?.send(event);
+          // } else if ( test_calculation_is_on === false) {
+          //   this.actor_react_running_s()?.send({ type: 'react.running.event.calculation.cancel'});
+          // }
+        });
 
-    // }, { allowSignalWrites: true})
+    }, { allowSignalWrites: true})
   }
 
   protected toggle_xstate( event: MatSlideToggleChange ) {
-    // this.actor_react_running_admin_toolbar_s()?.send({ type: 'react.admin_toolbox.event.x_state.toggle' });
+    this.$actor_admin_toolbar.send({ type: 'react.admin_toolbox.event.x_state.toggle' });
   }
   protected toggle_test_calculation( event: MatSlideToggleChange ) {
-    // this.actor_react_running_admin_toolbar_s()?.send({ type: 'react.admin_toolbox.event.test_calculation.toggle' });
+    this.$actor_admin_toolbar?.send({ type: 'react.admin_toolbox.event.test_calculation.toggle' });
   }
 }
