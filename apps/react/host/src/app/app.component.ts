694db4a821a4826af71da457fe39cc003230f166
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-22 06:59:32 +0100

    Remove navigation

--- apps/react/host/src/app/app.component.ts
+++ apps/react/host/src/app/app.component.ts
@@ -11,12 +11,6 @@ import { MatDialog, MatDialogRef } from '@angular/material/dialog';
   ],
   selector: 'kppk-react-root',
   template: `
-    <ul class="remote-menu">
-      <li><a routerLink="/">Home</a></li>
-      <li><a routerLink="react_view_login">ReactViewLogin</a></li>
-      <li><a routerLink="react_view_home">ReactViewHome</a></li>
-      <li><a routerLink="react_view_calc">ReactViewCalc</a></li>
-    </ul>
     <router-outlet/>
   `,
   styles: ``,
