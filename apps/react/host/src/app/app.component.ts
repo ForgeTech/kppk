2efb6846a4dedfaca2ec0bcfd72a18bf8957b629
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-22 12:20:44 +0100

    Updated

--- apps/react/host/src/app/app.component.ts
+++ apps/react/host/src/app/app.component.ts
@@ -17,31 +17,31 @@ import { MatDialog, MatDialogRef } from '@angular/material/dialog';
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  protected $icon = inject(KppkRegisterIconsService);
+  protected $actor_spinner = inject(FgSpinnerMachineActorService);
   protected $app = inject(AppService);
   protected $appRef = inject(ApplicationRef);
-  
+  protected $dialog = inject(MatDialog);
+  protected $icon = inject(KppkRegisterIconsService);
+
+  protected dialog_ref: MatDialogRef<any, any> | undefined;
+  protected dialog_toggleE = effect( () => {
+      const state = this.$actor_spinner.stateS();
+      if( state?.matches({DISPLAY: 'SHOWN'}) ) {
+        if( this.dialog_ref === undefined ) {
+          this.dialog_ref = this.$dialog.open(KppkReactLoadingIndicatorComponent, {
+            backdropClass: 'fg_spinner_modal_backdrop',
+            panelClass: 'fg_spinner_modal_panal',
+            enterAnimationDuration: '200ms',
+            exitAnimationDuration: '500ms',
+            autoFocus: true,
+            restoreFocus: true,
+            disableClose: true,
+          });
+        }
+      }
+      else if( state?.matches({DISPLAY: 'HIDDEN'}) || state?.matches({DISPLAY: 'DONE'})) {
+          this.dialog_ref?.close();
+      }
+  });
   public title = 'kppk-react';
-      protected $dialog = inject(MatDialog);
-      protected $actor_spinner = inject(FgSpinnerMachineActorService);
-      protected dialog_ref: MatDialogRef<any, any> | undefined;
-      protected dialog_toggleE = effect( () => {
-          const state = this.$actor_spinner.stateS();
-          if( state?.matches({DISPLAY: 'SHOWN'}) ) {
-            if( this.dialog_ref === undefined ) {
-              this.dialog_ref = this.$dialog.open(KppkReactLoadingIndicatorComponent, {
-                backdropClass: 'fg_spinner_modal_backdrop',
-                panelClass: 'fg_spinner_modal_panal',
-                enterAnimationDuration: '200ms',
-                exitAnimationDuration: '500ms',
-                autoFocus: true,
-                restoreFocus: true,
-                disableClose: true,
-              });
-            }
-          }
-          else if( state?.matches({DISPLAY: 'HIDDEN'}) || state?.matches({DISPLAY: 'DONE'})) {
-              this.dialog_ref?.close();
-          }
-      });
 }
