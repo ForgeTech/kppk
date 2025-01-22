7bcb3e2a2d6ccffdf355da165d8757d99ba14afc
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-22 07:08:06 +0100

    Added experimental decorator injecting environment services

---
+++ libs/fg-lib-new/src/lib/decorator/error-notification.decorator.ts
@@ -0,0 +1,35 @@
+import { getPlatform } from "@angular/core";
+import { fg_event_parser, FgEventService } from "../service";
+import { z } from "zod";
+
+export const fg_error_event_options_parser = fg_event_parser.extend({
+    type: z.string().default('fg.error.event'),
+    error: z.string().default('ERROR_EVENT'),
+    rethrow: z.boolean().default(true),
+});
+export type FG_ERROR_EVENT_OPTIONS = z.infer<typeof fg_error_event_options_parser>;
+
+export function fg_error_event( options: FG_ERROR_EVENT_OPTIONS ) {
+    return function(target: any, methode_name: string, descriptor: any): any {
+        const $event = getPlatform()?.injector.get(FgEventService);
+        const original_methode = descriptor.value;
+        descriptor.value = function(...args: any[]) {
+            try {
+                return original_methode.apply(this, args);
+            } catch( error: any ) {
+                $event?.emit({
+                    type: options.type,
+                    data: {
+                        ...options.data,
+                        error: options.error
+                    }
+                })
+                if(options.rethrow){
+                    throw error
+                }
+            } 
+            
+        }
+        return descriptor;
+    }
+}
