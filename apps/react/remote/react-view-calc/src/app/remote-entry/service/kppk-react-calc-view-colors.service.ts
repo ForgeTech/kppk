86df9df89b5750fa6d1b6d8f718e48d736cb0ac6
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-25 11:48:17 +0100

    Resolved warnings

--- apps/react/remote/react-view-calc/src/app/remote-entry/service/kppk-react-calc-view-colors.service.ts
+++ apps/react/remote/react-view-calc/src/app/remote-entry/service/kppk-react-calc-view-colors.service.ts
@@ -4,20 +4,20 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class KppkReactCalcViewColorsService  {
-  public absorbing: string = '#7CB342';
-  public emitting: string = '#FF3D00';
+  public absorbing = '#7CB342';
+  public emitting = '#FF3D00';
 
-  public construction_site: string = '#FF9800';
-  public container_village: string = '#FFC107';
-  public excavation_pit: string = '#FFEB3B';
-  public materials: string = '#9C27B0';
-  public demolish_disposal: string = '#3949AB';
-  public heating_system: string = '#E91E63';
+  public construction_site = '#FF9800';
+  public container_village = '#FFC107';
+  public excavation_pit = '#FFEB3B';
+  public materials = '#9C27B0';
+  public demolish_disposal = '#3949AB';
+  public heating_system = '#E91E63';
 
-  public transport: string = '#3949AB';
-  public creation: string = '#9C27B0';
-  public operation: string = '#E91E63';
+  public transport = '#3949AB';
+  public creation = '#9C27B0';
+  public operation = '#E91E63';
 
-  public concrete: string = '#FF3D00';
-  public concrete_oeko: string = '#7CB342'
+  public concrete = '#FF3D00';
+  public concrete_oeko = '#7CB342'
 }
