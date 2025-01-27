49b2ed761b4f470b937412c3332c16e82d5fe497
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-27 19:29:16 +0100

    Added vite test application setup for standalon react

---
+++ apps/kppk-react/src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideClientHydration(withEventReplay()),
+    // provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(appRoutes),
+  ],
+};
