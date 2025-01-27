8307e639f26c7cbd0c4f80e295b58bd6fad21387
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-27 19:17:21 +0100

    added language switch update

--- apps/react/remote/react-view-calc/src/app/remote-entry/entry.component.ts
+++ apps/react/remote/react-view-calc/src/app/remote-entry/entry.component.ts
@@ -4,8 +4,11 @@ import { MatStepperModule } from '@angular/material/stepper';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { NgObjectPipesModule } from 'ngx-pipes';
 import { 
+  FgAuthLocalMachineActorService,
+  FgAuthLocalMachineService,
   KppkFormlyModule,
-  // react_view_calculation_form_name_enum, 
+  REACT_VIEW_CALCULATION_FORM_NAME_ENUM,
+  ReactViewCalculationMachineActorService,
   // ReactViewCalculationMachineActorService 
 } from '@kppk/react-lib';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -36,7 +39,7 @@ import { FormlyFormOptions } from '@ngx-formly/core';
     KppkReactResultsComponent
   ],
   selector: 'kppk-react-view-calculation',
-  template: '<h1>./entry.component.html</h1>',
+  templateUrl: './entry.component.html',
   styleUrl: './entry.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
@@ -44,68 +47,68 @@ export class RemoteEntryComponent {
     protected $log = inject(NGXLogger);
     protected $fb = inject(FormBuilder);
     protected $active_route = inject(ActivatedRoute);
-    // protected $actor_view_calculation = inject(ReactViewCalculationMachineActorService)
+    protected $actor_view_calculation = inject(ReactViewCalculationMachineActorService);
+    protected $actor_auth = inject(FgAuthLocalMachineActorService);
   
+    protected $fields_common = inject(KppkReactCommonFields);
+    protected form_common = new FormGroup({});
+    protected fields_common = this.$fields_common.fields;
+    protected form_common_model_s = computed( () => {
+      const result = this.$actor_view_calculation.stateS()?.context.calculation?.form_common.value;    
+      this.$log.fatal('UPDATED_COMMON_MODEL');
+      this.$log.fatal(JSON.stringify( result ));
+      return result;
+    })
   
-    // protected $fields_common = inject(KppkReactCommonFields);
-    // protected form_common = new FormGroup({});
-    // protected fields_common = this.$fields_common.fields;
-    // protected form_common_model_s = computed( () => {
-    //   const result = this.$actor_view_calculation.stateS()?.context.calculation?.form_common.value;    
-    //   this.$log.fatal('UPDATED_COMMON_MODEL');
-    //   this.$log.fatal(JSON.stringify( result ));
-    //   return result;
-    // })
+    protected options_s = computed( () => {
+      const options: FormlyFormOptions = {
+        formState: { 
+          state_react_view_calculation: this.$actor_view_calculation.stateS()
+        }
+      }
+      return options;
+    })
   
-    // protected options_s = computed( () => {
-    //   const options: FormlyFormOptions = {
-    //     formState: { 
-    //       state_react_view_calculation: this.$actor_view_calculation.stateS()
-    //     }
-    //   }
-    //   return options;
-    // })
+    protected $fields_construction = inject(KppkReactConstructionSiteFields);
+    protected form_construction_site = new FormGroup({});
+    protected fields_construction_site = this.$fields_construction.fields();
+    protected form_construction_site_model_s = computed( () => {
+      const result = this.$actor_view_calculation.stateS()?.context.calculation?.form_construction_site.value;
+      // result.exact_energy_usage = []
+      return result;
+    })
   
-    // protected $fields_construction = inject(KppkReactConstructionSiteFields);
-    // protected form_construction_site = new FormGroup({});
-    // protected fields_construction_site = this.$fields_construction.fields();
-    // protected form_construction_site_model_s = computed( () => {
-    //   const result = this.$actor_view_calculation.stateS()?.context.calculation?.form_construction_site.value;
-    //   // result.exact_energy_usage = []
-    //   return result;
-    // })
+    protected $fields_container_village = inject(KppkReactContainerVillageFields);
+    protected form_container_village = new FormGroup({});
+    protected fields_container_village = this.$fields_container_village.fields;
+    protected form_container_village_model_s = computed( () => {
+      const result = this.$actor_view_calculation.stateS()?.context.calculation?.form_container_village.value;
+      return result;
+    })
   
-    // protected $fields_container_village = inject(KppkReactContainerVillageFields);
-    // protected form_container_village = new FormGroup({});
-    // protected fields_container_village = this.$fields_container_village.fields;
-    // protected form_container_village_model_s = computed( () => {
-    //   const result = this.$actor_view_calculation.stateS()?.context.calculation?.form_container_village.value;
-    //   return result;
-    // })
+    protected $fields_demolish_disposal = inject(KppkReactDemolishDisposalFields);
+    protected fields_demolish_disposal= this.$fields_demolish_disposal.demolish_disposal_fields;
+    protected form_demolish_disposal= new FormGroup({});
+    protected form_demolish_disposal_model_s = computed( () => {
+      const result = this.$actor_view_calculation.stateS()?.context.calculation?.form_demolish_disposal.value;
+      return result;
+    });
   
-    // protected $fields_demolish_disposal = inject(KppkReactDemolishDisposalFields);
-    // protected fields_demolish_disposal= this.$fields_demolish_disposal.demolish_disposal_fields;
-    // protected form_demolish_disposal= new FormGroup({});
-    // protected form_demolish_disposal_model_s = computed( () => {
-    //   const result = this.$actor_view_calculation.stateS()?.context.calculation?.form_demolish_disposal.value;
-    //   return result;
-    // });
+    protected $fields_excavation_pit = inject(KppkReactExcavationPitFields);
+    protected fields_excavation_pit = this.$fields_excavation_pit.excavation_pit_fields;
+    protected form_excavation_pit = new FormGroup({});
+    protected form_excavation_pit_model_s = computed( () => {
+      const result = this.$actor_view_calculation.stateS()?.context.calculation?.form_excavation_pit.value;
+      return result;
+    });
   
-    // protected $fields_excavation_pit = inject(KppkReactExcavationPitFields);
-    // protected fields_excavation_pit = this.$fields_excavation_pit.excavation_pit_fields;
-    // protected form_excavation_pit = new FormGroup({});
-    // protected form_excavation_pit_model_s = computed( () => {
-    //   const result = this.$actor_view_calculation.stateS()?.context.calculation?.form_excavation_pit.value;
-    //   return result;
-    // });
-  
-    // protected $fields_heating_system = inject(KppkReactHeatingSystemFields);
-    // protected fields_heating_system = this.$fields_heating_system.fields;
-    // protected form_heating_system = new FormGroup({});
-    // protected form_heating_system_model_s = computed( () => {
-    //   const result = this.$actor_view_calculation.stateS()?.context.calculation?.form_heating_system.value;
-    //   return result;
-    // });
+    protected $fields_heating_system = inject(KppkReactHeatingSystemFields);
+    protected fields_heating_system = this.$fields_heating_system.fields;
+    protected form_heating_system = new FormGroup({});
+    protected form_heating_system_model_s = computed( () => {
+      const result = this.$actor_view_calculation.stateS()?.context.calculation?.form_heating_system.value;
+      return result;
+    });
   
     protected form_step_selection = this.$fb.group({
       "material": [ true ],
@@ -115,127 +118,127 @@ export class RemoteEntryComponent {
       "excavation_pit": [ true ],
       "heating_system": [ true ],
     })
-    // protected form_step_selection_s = computed( () => {
-    //   const result = this.$actor_view_calculation.stateS()?.context.calculation?.form_step_selection.value;
-    //   return result;
-    // });
+    protected form_step_selection_s = computed( () => {
+      const result = this.$actor_view_calculation.stateS()?.context.calculation?.form_step_selection.value;
+      return result;
+    });
   
-    // protected results_s = computed(() => {
-    //   const result = this.$actor_view_calculation.stateS()?.context?.calculation;
-    //   return result;
-    // });
+    protected results_s = computed(() => {
+      const result = this.$actor_view_calculation.stateS()?.context?.calculation;
+      return result;
+    });
   
     constructor() {
-      // this.$actor_view_calculation.start()
       // super();
+      // this.$actor_view_calculation.start()
       // Update form_step_select with machine context values
-      // effect( () => {
-      //   const form_values = this.form_step_selection_s();
-      //   if(form_values) {
-      //     // this.form_step_selection.patchValue(form_values, {emitEvent: false})
-      //   }
-      // })
-      // effect(()=> {
-      //   console.log(this.state_react_view_calculation_s())
-      //   console.log('CHANGE');
+      effect( () => {
+        const form_values = this.form_step_selection_s();
+        if(form_values) {
+          // this.form_step_selection.patchValue(form_values, {emitEvent: false})
+        }
+      })
+    //   effect(()=> {
+    //     console.log(this.state_react_view_calculation_s())
+    //     console.log('CHANGE');
     // });
-      // effect(()=> {
-      //   console.log(this.options_s())
-      //   console.log('OPTIONS');
-      // });
+      effect(()=> {
+        console.log(this.options_s())
+        console.log('OPTIONS');
+      });
     }
   
-    // protected handle_common_form_change_event( event: any ) {
-    //   const event_to_dispatch = {
-    //     type: 'fg.form.common.event.update',
-    //     payload: {
-    //       name: react_view_calculation_form_name_enum.common,
-    //       errors: this.form_common.errors,
-    //       valid: this.form_construction_site.valid,
-    //       data: event
-    //     }
-    //   }
-    //   this.send_event_to_calculation_machine(event_to_dispatch);
-    // }
-    // protected handle_construction_site_form_change_event( event: any ) {
-    //   const event_to_dispatch = {
-    //     type: 'fg.form.construction_site.event.update',
-    //     payload: {
-    //       name: react_view_calculation_form_name_enum.construction_site,
-    //       errors: this.form_construction_site.errors,
-    //       valid: true,
-    //       data: event
-    //     }
-    //   }
-    //   this.send_event_to_calculation_machine(event_to_dispatch);
-    // }
-    // protected handle_container_village_form_change_event( event: any ) {
-    //   const event_to_dispatch = {
-    //     type: 'fg.form.container_village.event.update',
-    //     payload: {
-    //       name: react_view_calculation_form_name_enum.container_village,
-    //       errors: this.form_container_village.errors,
-    //       valid: this.form_container_village.valid,
-    //       data: event
-    //     }
-    //   }
-    //   this.send_event_to_calculation_machine(event_to_dispatch);
-    // }
-    // protected handle_demolish_disposal_form_change_event( event: any ) {
-    //   const event_to_dispatch = {
-    //     type: 'fg.form.demolish_disposal.event.update',
-    //     payload: {
-    //       name: react_view_calculation_form_name_enum.demolish_disposal,
-    //       errors: this.form_demolish_disposal.errors,
-    //       valid: this.form_demolish_disposal.valid,
-    //       data: event
-    //     }
-    //   }
-    //   this.send_event_to_calculation_machine(event_to_dispatch);
-    // }
-    // protected handle_excavation_pit_form_change_event( event: any ) {
-    //   const event_to_dispatch = {
-    //     type: 'fg.form.excavation_pit.event.update',
-    //     payload: {
-    //       name: react_view_calculation_form_name_enum.excavation_pit,
-    //       errors: this.form_excavation_pit.errors,
-    //       valid: this.form_excavation_pit.valid,
-    //       data: event
-    //     }
-    //   }
-    //   this.send_event_to_calculation_machine(event_to_dispatch);
-    // }
-    // protected handle_heating_system_form_change_event( event: any ) {
-    //   const event_to_dispatch = {
-    //     type: 'fg.form.heating_system.event.update',
-    //     payload: {
-    //       name: react_view_calculation_form_name_enum.heating_system,
-    //       errors: this.form_heating_system.errors,
-    //       valid: this.form_heating_system.valid,
-    //       data: event
-    //     }
-    //   }
-    //   this.send_event_to_calculation_machine(event_to_dispatch);
-    // }
-    // protected handle_form_step_selection_change_event( event: any ) {
-    //   const event_to_dispatch = {
-    //     type: 'fg.form.step_selection.event.update',
-    //     payload: {
-    //       name: react_view_calculation_form_name_enum.step_selection,
-    //       errors: this.form_step_selection.errors,
-    //       valid: this.form_step_selection.valid,
-    //       data: event
-    //     }
-    //   }
-    //   this.send_event_to_calculation_machine(event_to_dispatch);
-    // }
+    protected handle_common_form_change_event( event: any ) {
+      const event_to_dispatch = {
+        type: 'fg.form.common.event.update',
+        payload: {
+          name: REACT_VIEW_CALCULATION_FORM_NAME_ENUM.common,
+          errors: this.form_common.errors,
+          valid: this.form_construction_site.valid,
+          data: event
+        }
+      }
+      this.send_event_to_calculation_machine(event_to_dispatch);
+    }
+    protected handle_construction_site_form_change_event( event: any ) {
+      const event_to_dispatch = {
+        type: 'fg.form.construction_site.event.update',
+        payload: {
+          name: REACT_VIEW_CALCULATION_FORM_NAME_ENUM.construction_site,
+          errors: this.form_construction_site.errors,
+          valid: true,
+          data: event
+        }
+      }
+      this.send_event_to_calculation_machine(event_to_dispatch);
+    }
+    protected handle_container_village_form_change_event( event: any ) {
+      const event_to_dispatch = {
+        type: 'fg.form.container_village.event.update',
+        payload: {
+          name: REACT_VIEW_CALCULATION_FORM_NAME_ENUM.container_village,
+          errors: this.form_container_village.errors,
+          valid: this.form_container_village.valid,
+          data: event
+        }
+      }
+      this.send_event_to_calculation_machine(event_to_dispatch);
+    }
+    protected handle_demolish_disposal_form_change_event( event: any ) {
+      const event_to_dispatch = {
+        type: 'fg.form.demolish_disposal.event.update',
+        payload: {
+          name: REACT_VIEW_CALCULATION_FORM_NAME_ENUM.demolish_disposal,
+          errors: this.form_demolish_disposal.errors,
+          valid: this.form_demolish_disposal.valid,
+          data: event
+        }
+      }
+      this.send_event_to_calculation_machine(event_to_dispatch);
+    }
+    protected handle_excavation_pit_form_change_event( event: any ) {
+      const event_to_dispatch = {
+        type: 'fg.form.excavation_pit.event.update',
+        payload: {
+          name: REACT_VIEW_CALCULATION_FORM_NAME_ENUM.excavation_pit,
+          errors: this.form_excavation_pit.errors,
+          valid: this.form_excavation_pit.valid,
+          data: event
+        }
+      }
+      this.send_event_to_calculation_machine(event_to_dispatch);
+    }
+    protected handle_heating_system_form_change_event( event: any ) {
+      const event_to_dispatch = {
+        type: 'fg.form.heating_system.event.update',
+        payload: {
+          name: REACT_VIEW_CALCULATION_FORM_NAME_ENUM.heating_system,
+          errors: this.form_heating_system.errors,
+          valid: this.form_heating_system.valid,
+          data: event
+        }
+      }
+      this.send_event_to_calculation_machine(event_to_dispatch);
+    }
+    protected handle_form_step_selection_change_event( event: any ) {
+      const event_to_dispatch = {
+        type: 'fg.form.step_selection.event.update',
+        payload: {
+          name: REACT_VIEW_CALCULATION_FORM_NAME_ENUM.step_selection,
+          errors: this.form_step_selection.errors,
+          valid: this.form_step_selection.valid,
+          data: event
+        }
+      }
+      this.send_event_to_calculation_machine(event_to_dispatch);
+    }
   
     protected send_event_to_calculation_machine( event: any ) {
-      // this.actor_react_view_calculation_s()?.send(event)
+      this.$actor_view_calculation?.send(event)
      }
   
     protected logout( event?: Event ) {
       event?.preventDefault();
-      // this.actor_main_s()?.send({ type: 'fg.auth.local.event.logout' });
+      this.$actor_auth?.send({ type: 'fg.auth.local.event.logout' });
     };
 }
