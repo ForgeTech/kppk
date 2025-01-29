d1ea248018cf02c5b7fcbdb26f285a861f8c4114
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-29 08:07:12 +0100

    Added assets

---
+++ apps/kppk-react-tw/public/images/app/src/main-icon.ts
@@ -0,0 +1,5 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { IconModule } from './icon/icon.module';
+platformBrowserDynamic()
+  .bootstrapModule(IconModule)
+  .catch(err => console.error(err));
