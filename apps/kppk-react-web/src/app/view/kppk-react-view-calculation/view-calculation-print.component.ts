f06e6a3492a3dff2c4231787c6151a043ba64eee
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-29 12:40:37 +0100

    Removed web-project

--- apps/kppk-react-web/src/app/view/kppk-react-view-calculation/view-calculation-print.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-@Component({
-  imports: [CommonModule],
-  selector: 'kppk-react-view-calculation-print',
-  template: `<div>CALCULATION PRINT</div>`,
-})
-export class ViewCalculationPrintComponent {}
