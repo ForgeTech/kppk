b0818694aca4ec4dd4264cdeb5cccb78c05b17f9
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-29 12:43:26 +0100

    Removed not working esbuild project

--- apps/kppk-react/src/app/layout/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './kppk-react-default-layout/kppk-react-default-layout.component';
-export * from './kppk-react-default-layout/component/kppk-react-footer/kppk-react-footer.component';
-export * from './kppk-react-default-layout/component/kppk-react-header/kppk-react-header.component';
-export * from './kppk-react-default-layout/component/kppk-react-header-open-nav-btn/kppk-react-header-open-nav-btn.component';
-export * from './kppk-react-default-layout/component/kppk-react-navigation/kppk-react-navigation.component';
