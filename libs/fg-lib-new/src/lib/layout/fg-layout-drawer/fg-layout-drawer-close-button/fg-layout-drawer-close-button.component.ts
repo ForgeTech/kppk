4727d47208e2eead3d5ebab63de701291ece3812
Florian Neumann <di.neumann.florian@gmail.com>
2025-01-31 17:03:14 +0100

    Refector for nice close button

--- libs/fg-lib-new/src/lib/layout/fg-layout-drawer/fg-layout-drawer-close-button/fg-layout-drawer-close-button.component.ts
+++ libs/fg-lib-new/src/lib/layout/fg-layout-drawer/fg-layout-drawer-close-button/fg-layout-drawer-close-button.component.ts
@@ -3,7 +3,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { FgEventService } from '../../../service';
-import { FG_LAYOUT_DRAWER_CLOSE_OPTIONS, fg_layout_drawer_close_options_parser, fg_layout_drawer_event_open_parser } from '../fg-layout-drawer.type';
+import { FG_LAYOUT_DRAWER_CLOSE_OPTIONS, fg_layout_drawer_event_close_parser } from '../fg-layout-drawer.type';
 /**
  * FgLayoutDrawerCloseDrawerButtonComponent -
  * Component that can be used to dispatch a close-drawer event
@@ -16,14 +16,15 @@ import { FG_LAYOUT_DRAWER_CLOSE_OPTIONS, fg_layout_drawer_close_options_parser, 
     MatIconModule
   ],
   template: `
-    <button 
-      mat-icon-button
-      (click)="triggerDrawerClose($event)"
-      (keyup.enter)="triggerDrawerClose($event)"
+    <button
+      class="w-full"
+      mat-stroked-button
+      color="warn"
+      (click)="trigger_close($event)"
+      (keyup.enter)="trigger_close($event)"
     >
-      <ng-content>
-        <mat-icon>close</mat-icon>
-      </ng-content>
+      <mat-icon class="right-4">close</mat-icon>
+      {{ labelS() }}
     </button>
   `,
   styles: [''],
@@ -31,17 +32,15 @@ import { FG_LAYOUT_DRAWER_CLOSE_OPTIONS, fg_layout_drawer_close_options_parser, 
 })
 export class FgLayoutDrawerCloseButtonComponent {
   protected $event = inject(FgEventService, { optional: true })
-  /** Allows setting name used for source value */ 
-  public readonly nameS = input<string | undefined>(undefined, {alias: 'name'});
-  /** Allows to define the target layout of the open-drawer event (default: undefined)  */
-  public readonly targetS = input<string | undefined>(undefined, {alias: 'target'});
-  /** Outputs the options object */
-  public readonly eventO = output<FG_LAYOUT_DRAWER_CLOSE_OPTIONS>({alias: 'event'});
-  /** Methode used to trigger the dispatch of close drawer event */
-  public triggerDrawerClose(event: Event) {
-    event.preventDefault();
 
-    const event_to_dispatch = fg_layout_drawer_event_open_parser.parse({
+  public readonly nameS = input<string | undefined>(undefined, {alias: 'name'});
+  public readonly labelS = input<string | undefined>(undefined, {alias: 'label'});
+  public readonly targetS = input<string | undefined>(undefined, {alias: 'target'});
+  public readonly eventO = output<FG_LAYOUT_DRAWER_CLOSE_OPTIONS>({alias: 'event'});
+
+  public trigger_close(event: Event) {
+    event.preventDefault();
+    const event_to_dispatch = fg_layout_drawer_event_close_parser.parse({
         type: 'fg.layout.drawer.event.close', 
         source: this.nameS(), 
         target: this.targetS(),
