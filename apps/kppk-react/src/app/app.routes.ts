5ba2eedff5bc232e30afdd6381772a623323cf9f
Florian Neumann <di.neumann.florian@gmail.com>
2025-02-02 11:38:17 +0100

    Added login layout and minor fixes

--- apps/kppk-react/src/app/app.routes.ts
+++ apps/kppk-react/src/app/app.routes.ts
@@ -20,37 +20,49 @@ export const app_routes: Route[] = [
         loadComponent: () =>
           import(
             './view/kppk-react-view-calculation/kppk-react-view-calculation.component'
-          ).then((m) => m!.KppkReactViewCalculationComponent),
+          ).then((m) => m.KppkReactViewCalculationComponent),
       },
       {
         path: HOST_ROUTES.HOME,
         loadComponent: () =>
           import(
             './view/kppk-react-view-home/kppk-react-view-home.component'
-          ).then((m) => m!.KppkReactViewHomeComponent),
+          ).then((m) => m.KppkReactViewHomeComponent),
       },
       {
         path: HOST_ROUTES.IMPRINT,
         loadComponent: () =>
           import(
             './view/kppk-react-view-imprint/kppk-react-view-imprint.component'
-          ).then((m) => m!.KppkReactViewImprintComponent),
+          ).then((m) => m.KppkReactViewImprintComponent),
       },
       {
         path: HOST_ROUTES.DATA_PROTECTION,
         loadComponent: () =>
           import(
             './view/kppk-react-view-data-protection/kppk-react-view-data-protection.component'
-          ).then((m) => m!.KppkReactViewDataProtectionComponent),
+          ).then((m) => m.KppkReactViewDataProtectionComponent),
       },
     ],
   },
+  
   {
     path: HOST_ROUTES.LOGIN,
+
     loadComponent: () =>
       import(
-        './view/kppk-react-view-login/kppk-react-view-login.component'
-      ).then((m) => m!.KppkReactViewLoginComponent),
+        './layout/kppk-react-view-auth-layout-router-outlet/kppk-react-view-auth-layout-router-outlet.component'
+      ).then((m) => m.KppkReactViewAuthLayoutRouterOutletComponent),
+      children: [
+        {
+          path: HOST_ROUTES.ROOT,
+          pathMatch: 'prefix',
+          loadComponent: () =>
+            import(
+              './view/kppk-react-view-auth-login/kppk-react-view-auth-login.component'
+            ).then((m) => m.KppkReactViewLoginComponent),
+        }
+      ]
   },
   {
     path: HOST_ROUTES.WILDCARD,
