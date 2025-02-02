4cb3a97fdc0578906b763c393503dfdf7cffcda0
Florian Neumann <di.neumann.florian@gmail.com>
2025-02-02 11:41:20 +0100

    Added load_object utility

--- libs/react-lib/src/lib/machine/machine.utils.ts
+++ libs/react-lib/src/lib/machine/machine.utils.ts
@@ -1,3 +1,7 @@
+import { HttpClient } from "@angular/common/http";
+import { getPlatform } from "@angular/core";
+import { forkJoin, Observable } from "rxjs";
+
 export const parent_context_event_input = <T, E>({
   context,
   event,
@@ -10,6 +14,7 @@ export const parent_context_event_input = <T, E>({
   // console.log( event )
   return { context, event };
 };
+
 export const parent_context_event_input_test = <T, E>({
   context,
   event,
@@ -22,3 +27,17 @@ export const parent_context_event_input_test = <T, E>({
   // console.log( event )
   return { context, event };
 };
+
+export function load_object(to_load: Record<string, string>) {
+  const $http = getPlatform()?.injector.get(HttpClient);
+  const resources: Record<string, Observable<any>> = {};
+  const sources = Object.keys(to_load).forEach((key) => {
+    if( $http ) {
+      resources[key] = $http.get(to_load[key]);
+    } else {
+      // TODO
+      throw new Error(`ERROR: machine_utils > load_object requires angular 'getPlattform' to provide HttpClient`);
+    } 
+  });
+  return forkJoin(resources);
+}
