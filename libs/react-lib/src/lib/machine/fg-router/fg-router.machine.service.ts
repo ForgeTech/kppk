e41d3f53d45e678a46b32dac3d22d269029e1e00
Florian Neumann <di.neumann.florian@gmail.com>
2025-02-05 19:00:33 +0100

    Updates router service

--- libs/react-lib/src/lib/machine/fg-router/fg-router.machine.service.ts
+++ libs/react-lib/src/lib/machine/fg-router/fg-router.machine.service.ts
@@ -5,7 +5,7 @@ import {
   NavigationStart,
   Router,
 } from '@angular/router';
-import { filter, map, merge, Observable } from 'rxjs';
+import { filter, map, merge, Observable, of, switchMap } from 'rxjs';
 import { EventObject } from 'xstate';
 import { FgXstateService } from '../../service';
 import { z } from 'zod';
@@ -20,7 +20,7 @@ export type FG_ROUTER_CONTEXT = z.infer<typeof fg_router_context_parser>;
 
 export const fg_navigation_event_start_parser = fg_event_parser.extend({
   type: z.literal("fg.navigation.event.start"),
-  date: z.object({ 
+  data: z.object({ 
     url: z.string()
   })
 });
@@ -28,7 +28,7 @@ export type FG_NAVIGATION_EVENT_START = z.infer<typeof fg_navigation_event_start
 
 export const fg_navigation_event_end_parser = fg_event_parser.extend({
   type: z.literal("fg.navigation.event.end"),
-  date: z.object({ 
+  data: z.object({ 
     url: z.string(),
     url_after_redirects: z.string()
   })
@@ -37,7 +37,7 @@ export type FG_NAVIGATION_EVENT_END = z.infer<typeof fg_navigation_event_end_par
 
 export const fg_navigation_event_cancel_parser = fg_event_parser.extend({
   type: z.literal("fg.navigation.event.cancel"),
-  date: z.object({ 
+  data: z.object({ 
     url: z.string(),
     reason: z.string()
   })
@@ -52,39 +52,46 @@ export class FgRouterMachineService {
   protected $xstate = inject(FgXstateService);
 
   public getMachine(config?: FG_ROUTER_CONTEXT) {
-    const parsed_config = fg_router_context_parser.parse(config);
-    const router$: Observable<EventObject> = merge(
-      this.$router.events.pipe(
-        filter((event) => (event instanceof NavigationStart ? true : false)),
-        map((event) => {
-          const { url } = event as NavigationStart;
-          return fg_navigation_event_start_parser.parse({ 
-            type: 'fg.navigation.event.start',
-            ...parsed_config,
-            data: { url } 
-          });
-      })),
-      this.$router.events.pipe(
-        filter((event) => (event instanceof NavigationEnd ? true : false)),
-        map((event) => {
-          const { url, urlAfterRedirects } = event as NavigationEnd;
-          return fg_navigation_event_end_parser.parse({
-            type: 'fg.navigation.event.end',
-            ...parsed_config,
-            data: { url, url_after_redirects: urlAfterRedirects },
-          });
-      })),
-      this.$router.events.pipe(
-        filter((event) => (event instanceof NavigationCancel ? true : false)),
-        map((event) => {
-          const { url, reason } = event as NavigationCancel;
-          return fg_navigation_event_cancel_parser.parse({ 
-            type: 'fg.navigation.event.cancel', 
-            ...parsed_config,
-            data: { url, reason } 
-          });
-      }))
-    );
+    const parsed_config = fg_router_context_parser.parse(config ?? {});
+
+
+    const router$: Observable<EventObject> = of(parsed_config).pipe(switchMap( config => {
+      return merge(
+        this.$router.events.pipe(
+          filter((event) => (event instanceof NavigationStart ? true : false)),
+          map((event) => {
+            const { url } = event as NavigationStart;
+            const result = fg_navigation_event_start_parser.parse({ 
+              type: 'fg.navigation.event.start',
+              ...config,
+              data: { url } 
+            });
+            return result;
+        })),
+        this.$router.events.pipe(
+          filter((event) => (event instanceof NavigationEnd ? true : false)),
+          map((event) => {
+            const { url, urlAfterRedirects } = event as NavigationEnd;
+            const result = fg_navigation_event_end_parser.parse({
+              type: 'fg.navigation.event.end',
+              ...config,
+              data: { url, url_after_redirects: urlAfterRedirects },
+            });
+            return result;
+        })),
+        this.$router.events.pipe(
+          filter((event) => (event instanceof NavigationCancel ? true : false)),
+          map((event) => {
+            const { url, reason } = event as NavigationCancel;
+            const result = fg_navigation_event_cancel_parser.parse({ 
+              type: 'fg.navigation.event.cancel', 
+              ...config,
+              data: { url, reason } 
+            });
+            return result;
+        }))
+      );
+    })) 
     return this.$xstate.fromEventObservable(() => router$);
   }
 }
