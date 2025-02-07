ecc40e5fd6d10f604a42576ee8a9c9b9a61d9843
Florian Neumann <di.neumann.florian@gmail.com>
2025-02-07 08:30:08 +0100

    Finished work on logout view

--- apps/kppk-react/src/app/view/kppk-react-view-auth-logout/kppk-react-view-auth-logout.component.ts
+++ apps/kppk-react/src/app/view/kppk-react-view-auth-logout/kppk-react-view-auth-logout.component.ts
@@ -1,19 +1,19 @@
-import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, signal, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslocoService } from '@jsverse/transloco';
-import { FormGroup } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import {
+  FG_AUTH_LOCAL_EVENT_LOGOUT,
   FgAuthLocalMachineActorService,
   KppkFormlyModule,
-  KppkReactSharedService,
+  KppkReactLoadingIndicatorComponent,
 } from '@kppk/react-lib';
-import { map } from 'rxjs';
+import { delay, filter, map, tap } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { HOST_ROUTES } from '@kppk/react-lib';
 import { KppkReactViewAuthLayoutContentComponent } from '../../layout';
-import { FgTranslate } from '@kppk/fg-lib-new';
+import { FgEnvironmentService, FgTranslate } from '@kppk/fg-lib-new';
+import { Router } from '@angular/router';
 
 @Component({
   imports: [
@@ -21,45 +21,75 @@ import { FgTranslate } from '@kppk/fg-lib-new';
     KppkFormlyModule,
     MatButtonModule,
     MatIconModule,
-    KppkReactViewAuthLayoutContentComponent
+    KppkReactViewAuthLayoutContentComponent,
+    KppkReactLoadingIndicatorComponent
   ],
   selector: 'kppk-react-view-auth-logout',
   templateUrl: './kppk-react-view-auth-logout.component.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class KppkReactViewAuthLogoutComponent {
   protected HOST_ROUTES = HOST_ROUTES;
   protected $translate = inject(FgTranslate);
-  protected $auth_actor = inject(FgAuthLocalMachineActorService);
+  protected $actor_auth = inject(FgAuthLocalMachineActorService);
+  protected $env = inject(FgEnvironmentService);
+  protected $router = inject(Router);
   protected translations$ = this.$translate.get_translations$({
-    "headline_auth_logout": "auth", 
     "error_auth_logout": "auth", 
-    "success_auth_logout": "auth", 
+    "headline_auth_logout": "auth", 
     "label_back": "general", 
-    "label_send": "general", 
+    "label_retry": "general", 
+    "success_auth_logout_text": "auth", 
+    "success_auth_logout": "auth", 
+    "text_auth_logout_error": "auth",
+    "text_auth_logout_pending": "auth",
+    "text_auth_logout_success": "auth",
   });
   protected translationsS = toSignal(
     this.translations$,
     { initialValue: undefined }
   );
+  protected errorS = toSignal(
+    this.$actor_auth.state$.pipe(
+      map(snapshot => snapshot.matches({ 'STATE':{'AUTHORIZED': 'ERROR' }}))
+    ), 
+    { initialValue: false}
+  );
+  protected show_successS = signal(false);
+  protected successS = toSignal(
+    this.$actor_auth.state$.pipe(
+      map(snapshot => snapshot.matches({'STATE': 'UNAUTHORIZED'})),
+      filter( is_success => is_success ? true : false),
+      tap( is_success => this.show_successS.set(is_success)),
+      delay(3500)
+    ), 
+    { initialValue: false}
+  );
+  protected successE = effect( () => {
+    if( this.successS() ) {
+      this.$router.navigate([HOST_ROUTES.AUTH, HOST_ROUTES.AUTH_LOGIN]);
+    }
+  })
+  protected pendingS = toSignal(
+    this.$actor_auth.state$.pipe(
+      map(snapshot => snapshot.matches({ 'STATE':{'AUTHORIZED': 'REVOKE_AUTHORIZATION' }}))
+    ), 
+    { initialValue: true }
+  );
 
-  protected errorS = computed( () => {
-    return false;
-  })
-  protected successS = computed( () => {
-    return false;
-  })
-  protected pendingS = computed( () => {
-    return false;
-  })
+  constructor() {
+    if( !this.$env.development?.enabled ) {
+      this.logout();
+    }
+  }
 
-  protected action(event?: Event) {
-    // event?.preventDefault();
-    // const event_to_dispatch = {
-    //   type: 'fg.password_change.event.' as const,
-    //   payload: this.form.value
-    // };
-    // this.$auth_actor.send(event_to_dispatch);
+  protected logout(event?: Event) {
+    event?.preventDefault();
+    const event_to_dispatch: FG_AUTH_LOCAL_EVENT_LOGOUT = {
+      type: 'fg.auth.local.event.logout'
+    };
+    this.$actor_auth.send(event_to_dispatch);
   }
 }
