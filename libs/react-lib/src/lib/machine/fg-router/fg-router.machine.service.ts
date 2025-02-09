fa3ba5698c11889be6a137afb42be6d50963e9b9
Florian Neumann <di.neumann.florian@gmail.com>
2025-02-09 10:28:18 +0100

    Changes to router emitter

--- libs/react-lib/src/lib/machine/fg-router/fg-router.machine.service.ts
+++ libs/react-lib/src/lib/machine/fg-router/fg-router.machine.service.ts
@@ -18,31 +18,31 @@ export const fg_router_context_parser = z.object({
 });
 export type FG_ROUTER_CONTEXT = z.infer<typeof fg_router_context_parser>;
 
-export const fg_navigation_event_start_parser = fg_event_parser.extend({
-  type: z.literal("fg.navigation.event.start"),
+export const fg_router_emitted_start_parser = fg_event_parser.extend({
+  type: z.literal("fg.router.emitted.start"),
   data: z.object({ 
     url: z.string()
   })
 });
-export type FG_NAVIGATION_EVENT_START = z.infer<typeof fg_navigation_event_start_parser>;
+export type FG_ROUTER_EMITTED_START = z.infer<typeof fg_router_emitted_start_parser>;
 
-export const fg_navigation_event_end_parser = fg_event_parser.extend({
-  type: z.literal("fg.navigation.event.end"),
+export const fg_router_emitted_end_parser = fg_event_parser.extend({
+  type: z.literal("fg.router.emitted.end"),
   data: z.object({ 
     url: z.string(),
     url_after_redirects: z.string()
   })
 });
-export type FG_NAVIGATION_EVENT_END = z.infer<typeof fg_navigation_event_end_parser>;
+export type FG_ROUTER_EMITTED_END = z.infer<typeof fg_router_emitted_end_parser>;
 
-export const fg_navigation_event_cancel_parser = fg_event_parser.extend({
-  type: z.literal("fg.navigation.event.cancel"),
+export const fg_router_emitted_cancel_parser = fg_event_parser.extend({
+  type: z.literal("fg.router.emitted.cancel"),
   data: z.object({ 
     url: z.string(),
     reason: z.string()
   })
 });
-export type FG_NAVIGATION_EVENT_CANCEL = z.infer<typeof fg_navigation_event_cancel_parser>;
+export type FG_ROUTER_EMITTED_CANCEL = z.infer<typeof fg_router_emitted_cancel_parser>;
 
 @Injectable({
   providedIn: 'root',
@@ -61,33 +61,33 @@ export class FgRouterMachineService {
           filter((event) => (event instanceof NavigationStart ? true : false)),
           map((event) => {
             const { url } = event as NavigationStart;
-            const result = fg_navigation_event_start_parser.parse({ 
-              type: 'fg.navigation.event.start',
+            const result = fg_router_emitted_start_parser.parse({ 
+              type: 'fg.router.emitted.start',
               ...config,
               data: { url } 
-            });
+            } as FG_ROUTER_EMITTED_START);
             return result;
         })),
         this.$router.events.pipe(
           filter((event) => (event instanceof NavigationEnd ? true : false)),
           map((event) => {
             const { url, urlAfterRedirects } = event as NavigationEnd;
-            const result = fg_navigation_event_end_parser.parse({
-              type: 'fg.navigation.event.end',
+            const result = fg_router_emitted_end_parser.parse({
+              type: 'fg.router.emitted.end',
               ...config,
               data: { url, url_after_redirects: urlAfterRedirects },
-            });
+            } as FG_ROUTER_EMITTED_END);
             return result;
         })),
         this.$router.events.pipe(
           filter((event) => (event instanceof NavigationCancel ? true : false)),
           map((event) => {
             const { url, reason } = event as NavigationCancel;
-            const result = fg_navigation_event_cancel_parser.parse({ 
-              type: 'fg.navigation.event.cancel', 
+            const result = fg_router_emitted_cancel_parser.parse({ 
+              type: 'fg.router.emitted.cancel', 
               ...config,
               data: { url, reason } 
-            });
+            } as FG_ROUTER_EMITTED_CANCEL);
             return result;
         }))
       );
