748460dbce2cf205d879e13a25a99fcd2eb7027d
Florian Neumann <di.neumann.florian@gmail.com>
2025-02-22 16:50:11 +0100

    Fixed translation

--- apps/kppk-react/src/app/layout/kppk-react-default-layout/component/kppk-react-header/kppk-react-header.component.ts
+++ apps/kppk-react/src/app/layout/kppk-react-default-layout/component/kppk-react-header/kppk-react-header.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, ViewEncapsulation, computed, inject } from '@angular/core';
-import { FgLayoutDrawerOpenButtonComponent } from '@kppk/fg-lib-new';
+import { FgLayoutDrawerOpenButtonComponent, FgTranslate } from '@kppk/fg-lib-new';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,6 +9,7 @@ import { CommonModule } from '@angular/common';
 import { FgAuthLocalMachineActorService, FgSpinnerMachineActorService, HOST_ROUTES } from '@kppk/react-lib';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 /**
  * RoseHeaderComponent -
@@ -34,10 +35,21 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 export class KppkReactHeaderComponent {
   protected $actor_auth = inject(FgAuthLocalMachineActorService);
   protected $actor_spinner = inject(FgSpinnerMachineActorService);
+  protected $translate = inject(FgTranslate);
   protected HOST_ROUTES = HOST_ROUTES;
   public $element_ref = inject(ElementRef);
 
- protected auth_is_authorizedS = computed(() => {
+  protected translationsS = toSignal(
+    this.$translate.get_translations$({
+      "alt_react_logo": "general",
+      "label_login": "general",
+      "label_logout": "general",
+      "label_print": "general",
+    }),
+    { initialValue: undefined }
+  );
+
+  protected auth_is_authorizedS = computed(() => {
     return this.$actor_auth.stateS()?.matches({ STATE: 'AUTHORIZED' }) ?? false;
   }); 
 
