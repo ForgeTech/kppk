c9b8b1a139e17ef83f8bb6474bf125fa3e4d1c8b
Florian Neumann <di.neumann.florian@gmail.com>
2025-02-25 08:08:45 +0100

    Added input and changed formatting

--- libs/react-lib/src/lib/machine/react-view-calculation-material/react-view-calculation-material.machine.service.ts
+++ libs/react-lib/src/lib/machine/react-view-calculation-material/react-view-calculation-material.machine.service.ts
@@ -3,18 +3,13 @@ import { Injectable, inject } from '@angular/core';
 import {
   react_calculation_materials_context,
   REACT_CALCULATION_MATERIALS_CONTEXT,
+  REACT_CALCULATION_MATERIALS_FILE_DATA,
   react_calculation_materials_output,
   REACT_CALCULATION_MATERIALS_OUTPUT,
 } from '../../types';
 import { FgXstateService } from '../../service';
 import { ReactCalculationMaterialMachineMethodeService } from './react-view-calculation-material.machine.methode.service';
 import { parent_context_event_input } from '../machine.utils';
-
-// export const react_calculation_materials_v1_input = z.object({
-//   constext: react_calculation_materials_v1_context,
-//   event: z.any()
-// });
-// export type ReactCalculationMaterialsV1Context = z.infer<typeof react_calculation_materials_v1_context>;
 
 @Injectable({
   providedIn: 'root',
@@ -23,53 +18,38 @@ export class ReactCalculationMaterialService extends FgBaseService {
   protected $methode = inject(ReactCalculationMaterialMachineMethodeService);
   protected $xstate = inject(FgXstateService);
 
-  public get_machine(context?: REACT_CALCULATION_MATERIALS_CONTEXT) {
+  public get_machine() {
     return this.$xstate
       .setup({
         types: {
+          input: {} as REACT_CALCULATION_MATERIALS_FILE_DATA,
           context: {} as REACT_CALCULATION_MATERIALS_CONTEXT,
           output: {} as REACT_CALCULATION_MATERIALS_OUTPUT,
         },
         actions: {
-          assign_oi3_transform_result: this.$xstate.assign(
-            this.$methode.assign_oi3_transform_result
-          ),
-          assign_bauteilflaechen_transform_result: this.$xstate.assign(
-            this.$methode.assign_bauteilflaechen_transform_result
-          ),
-          assign_aufbauten_transform_result: this.$xstate.assign(
-            this.$methode.assign_aufbauten_transform_result
-          ),
-          assign_change_aufbauten: this.$xstate.assign(
-            this.$methode.assign_change_aufbauten
-          ),
-          assign_change_bauteilflaechen: this.$xstate.assign(
-            this.$methode.assign_change_bauteilflaechen
-          ),
-          assign_change_oi3: this.$xstate.assign(
-            this.$methode.assign_change_oi3
-          ),
-          assign_merge_arich_oi3: this.$xstate.assign(
-            this.$methode.assign_merge_arich_oi3
-          ),
-          assign_material_type: this.$xstate.assign(
-            this.$methode.assign_material_type
-          ),
+          assign_oi3_transform_result: this.$xstate.assign( this.$methode.assign_oi3_transform_result ),
+          assign_bauteilflaechen_transform_result: this.$xstate.assign( this.$methode.assign_bauteilflaechen_transform_result ),
+          assign_aufbauten_transform_result: this.$xstate.assign( this.$methode.assign_aufbauten_transform_result ),
+          assign_change_aufbauten: this.$xstate.assign( this.$methode.assign_change_aufbauten ),
+          assign_change_bauteilflaechen: this.$xstate.assign( this.$methode.assign_change_bauteilflaechen ),
+          assign_change_oi3: this.$xstate.assign( this.$methode.assign_change_oi3 ),
+          assign_merge_arich_oi3: this.$xstate.assign( this.$methode.assign_merge_arich_oi3 ),
+          assign_material_type: this.$xstate.assign( this.$methode.assign_material_type ),
         },
         actors: {
-          actor_tranform_oi3_file_inputs: this.$xstate.fromPromise(
-            this.$methode.actor_tranform_oi3_file_inputs
-          ),
-          actor_tranform_bauteilflaechen_file_inputs: this.$xstate.fromPromise(
-            this.$methode.actor_tranform_bauteilflaechen_file_inputs
-          ),
-          actor_tranform_aufbauten_file_inputs: this.$xstate.fromPromise(
-            this.$methode.actor_tranform_aufbauten_file_inputs
-          ),
+          actor_tranform_oi3_file_inputs: this.$xstate.fromPromise( this.$methode.actor_tranform_oi3_file_inputs ),
+          actor_tranform_bauteilflaechen_file_inputs: this.$xstate.fromPromise( this.$methode.actor_tranform_bauteilflaechen_file_inputs ),
+          actor_tranform_aufbauten_file_inputs: this.$xstate.fromPromise( this.$methode.actor_tranform_aufbauten_file_inputs ),
         },
       })
       .createMachine({
-        context: react_calculation_materials_context.parse(context ?? {}),
+        context: ({input }) => {
+          const context = {
+            input,
+            output: {}
+          } 
+          return react_calculation_materials_context.parse( context );
+        },
         output: ({
           context,
         }: {
