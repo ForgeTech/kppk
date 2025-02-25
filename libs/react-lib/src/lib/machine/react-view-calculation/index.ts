f37cd738384fd0f226d95eeec85aed01aaf47923
Florian Neumann <di.neumann.florian@gmail.com>
2025-02-25 08:28:04 +0100

    Added main machine actor service

--- libs/react-lib/src/lib/machine/react-view-calculation/index.ts
+++ libs/react-lib/src/lib/machine/react-view-calculation/index.ts
@@ -1,3 +1,4 @@
+export * from './react-view-calculation-main.machine.actor.service';
 export * from './react-view-calculation.machine.actor.service';
 export * from './react-view-calculation.machine.methode.service';
 export * from './react-view-calculation.machine.service';
