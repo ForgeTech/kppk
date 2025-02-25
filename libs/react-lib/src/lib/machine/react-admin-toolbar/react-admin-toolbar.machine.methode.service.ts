926840bc47b2df8ef360719fa8c6c6804b3bd478
Florian Neumann <di.neumann.florian@gmail.com>
2025-02-25 08:11:14 +0100

    Imported common utils for load_object

--- libs/react-lib/src/lib/machine/react-admin-toolbar/react-admin-toolbar.machine.methode.service.ts
+++ libs/react-lib/src/lib/machine/react-admin-toolbar/react-admin-toolbar.machine.methode.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, inject } from '@angular/core';
 import { FgImmutableService } from '../../service';
 
-import { load_admin_data_parser, REACT_ADMIN_TOOLBAR_CONTEXT, REACT_ADMIN_TOOLBAR_INPUT } from './react-admin-toolbar.machine.types';
+import { load_admin_data_parser, REACT_ADMIN_TOOLBAR_CONTEXT } from './react-admin-toolbar.machine.types';
 import { boundMethod } from 'autobind-decorator';
-import { fg_error_event, FgBaseService, FgEnvironmentService, FgEventService, FgStorageService } from '@kppk/fg-lib-new';
-import { firstValueFrom, forkJoin, Observable } from 'rxjs';
+import { FgBaseService, FgEnvironmentService, FgEventService, FgStorageService } from '@kppk/fg-lib-new';
+import { firstValueFrom, forkJoin } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { FgMachineUtilsMethodeService } from '../fg-machine-utils';
 
 export const ADMIN_TOOLBAR_SETTINGS_STORAGE_KEY = 'react_admin_toolbar_settings';
 
@@ -18,10 +19,11 @@ export class ReactAdminToolbarMachineMethodeService extends FgBaseService {
   protected $env = inject(FgEnvironmentService);
   protected $storage = inject(FgStorageService);
   protected $event = inject(FgEventService);
+  protected $common = inject(FgMachineUtilsMethodeService);
 
   @boundMethod
   public async actor_load_admin_data() {
-    const debug_calculation_v1$ = this.load_object({
+    const debug_calculation_v1$ = this.$common.load_object({
       file_aufbauten:
         './react/data/august2024/debug_calculation_v1/file_aufbauten.json',
       file_bauteilflaechen:
@@ -65,12 +67,4 @@ export class ReactAdminToolbarMachineMethodeService extends FgBaseService {
     return result;
   }
 
-  @boundMethod  
-  private load_object(to_load: Record<string, string>) {
-    const resources: Record<string, Observable<any>> = {};
-    Object.keys(to_load).forEach((key) => {
-        resources[key] = this.$http.get(to_load[key]);
-    });
-    return forkJoin(resources);
-  }
 }
