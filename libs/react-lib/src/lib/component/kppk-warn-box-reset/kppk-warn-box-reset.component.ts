3d03fdb71564493925d2a4296c151d8cb6af9154
Florian Neumann <di.neumann.florian@gmail.com>
2025-02-26 11:10:40 +0100

    Fix translations

--- libs/react-lib/src/lib/component/kppk-warn-box-reset/kppk-warn-box-reset.component.ts
+++ libs/react-lib/src/lib/component/kppk-warn-box-reset/kppk-warn-box-reset.component.ts
@@ -1,25 +1,36 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  inject,
   ViewEncapsulation,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FgTranslate } from '@kppk/fg-lib-new';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'kppk-warn-box-reset',
 
   imports: [CommonModule],
   template: `
+    @let t = translationS();
     <div
       class="border-l-4 border-yellow-500 bg-yellow-100 p-2 text-yellow-700"
       role="alert"
     >
-      <p class="font-bold">{{ 'calc.caution' }}</p>
-      <p>{{ 'calc.caution_resets_following' }}</p>
+      <p class="font-bold">{{ t?.caution }}</p>
+      <p>{{ t?.caution_resets_following }}</p>
     </div>
   `,
   styles: [],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class KppkWarnBoxResetComponent {}
+export class KppkWarnBoxResetComponent {
+  protected $translate = inject(FgTranslate);
+  protected translation$ = this.$translate.get_translations$({
+    "caution": "calc",
+    "caution_resets_following": "calc",
+  });
+  protected translationS = toSignal(this.translation$, {initialValue: undefined})
+}
