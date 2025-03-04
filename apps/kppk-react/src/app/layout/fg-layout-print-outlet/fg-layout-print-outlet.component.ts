2549110dac12972da0ce34c3a134864a7aee70f1
Florian Neumann <di.neumann.florian@gmail.com>
2025-03-04 13:04:29 +0100

    WIP fixes for faulty assumptions

--- apps/kppk-react/src/app/layout/fg-layout-print-outlet/fg-layout-print-outlet.component.ts
+++ apps/kppk-react/src/app/layout/fg-layout-print-outlet/fg-layout-print-outlet.component.ts
@@ -6,20 +6,17 @@ import { RouterModule } from '@angular/router';
   imports: [RouterModule, NgClass],
   selector: 'fg-layout-print-outlet',
   template: `
-    <section
-      class="page-content"
-    >
-      <!-- [ngClass]="{
-        invisible: print_outlet_activatedS() === true
-      }" -->
-      <router-outlet class="content-outlet" />
-    </section>
+  <div
+    [ngClass]="{
+      'is-printing': is_printingS() === false
+    }"
+  >
     <section
       class="print-content"
-    >
-      <!-- [ngClass]="{
+      [ngClass]="{
         invisible: print_outlet_activatedS() === false
-      }" -->
+      }"
+    >
       <router-outlet
         class="print-outlet"
         name="print_outlet"
@@ -27,10 +24,17 @@ import { RouterModule } from '@angular/router';
         (deactivate)="print_outlet_activatedS.set(false)"
       />
     </section>
+    <section
+      class="page-content"
+    >
+      <router-outlet class="content-outlet" />
+    </section>
+  </div>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
 })
 export class FgLayoutPrintOutletComponent {
   protected print_outlet_activatedS = signal(false);
+  protected is_printingS = signal(true);
 }
